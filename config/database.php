640626c5fc3fb094be34ddfe1bf9b87d4854aea1
Andrew Melo <andrew.melo@gmail.com>
2011-08-23 00:52:54 -0500

    initial commit

---
+++ config/database.php
@@ -0,0 +1,13 @@
+<?php
+class DATABASE_CONFIG {
+
+	var $default = array(
+		'driver' => 'mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'melo_ec2',
+		'password' => 'trURaXExuqupr&sW2guWaQad9MAseCha',
+		'database' => 'melo_ec2',
+	);
+}
+?>
